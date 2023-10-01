8ea9c60b70810e07bb7b65957938b4cbfbc6828b
denysbandrivskyi <denys.bandrivskyi.ri.2023@lpnu.ua>
2023-10-02 00:22:53 +0300

    first commit

---
+++ laby/lab_2.1.cpp
@@ -0,0 +1,31 @@
+﻿// Lab_02.cpp
+// < Бандрівський Денис >
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 1
+
+#include <iostream>
+#include <windows.h>
+
+#pragma execution_character_set( "utf-8" )
+
+using namespace std;
+
+int main() {
+	SetConsoleOutputCP(65001);
+	double z1;
+	double z2;
+	double a;
+	double Pi = 4 * atan(1.);
+
+	cout << "Pi number : " << Pi << endl;
+
+	cout << "Enter the number a: "; cin >> a;
+
+	z1 = (2. * pow(sin((3. * Pi) - (2. * a)), 2)) * (pow(cos((5. * Pi) + (2. * a)), 2));
+	z2 = 1. / 4 - 1. / 4 * sin(5. / 2 * Pi - 8 * a);
+
+	cout << "If a = " << a << endl << "z1 = " << z1 << endl << "z2 = " << z2 << endl;
+
+	return 0;
+}
